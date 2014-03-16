2797d9944cc04565638c8ed884c644f20174d53d
Amir Kurtovic <amirkurtovic@gmail.com>
2014-03-15 22:09:00 -0500

    Added load() to dictionary.h

--- pset6/dictionary.c
+++ pset6/dictionary.c
@@ -8,7 +8,10 @@
  ***************************************************************************/
 
 #include <stdbool.h>
-
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
 #include "dictionary.h"
 
 /**
@@ -16,7 +19,6 @@
  */
 bool check(const char* word)
 {
-    // TODO
     return false;
 }
 
@@ -25,24 +27,71 @@ bool check(const char* word)
  */
 bool load(const char* dictionary)
 {
-    // TODO
-    return false;
+    FILE *fp = fopen(dictionary, "r");
+    if (fp == NULL)
+    {
+        return false;
+    }
+
+    // malloc memory for firstNade
+    firstNode = malloc(sizeof(trie));
+    int character = 0;
+    
+    // use trie* currentNode as cursor
+    trie* currentNode = NULL;
+
+	//loop through dictionary until end of file EOF
+    while(fgetc(fp) != EOF)
+    {
+        fseek(fp, -1, SEEK_CUR);
+
+        // set cursor to firstNode
+        currentNode = firstNode;
+
+		//loop through characters until \n
+        for (character = fgetc(fp); character != '\n'; 
+                character = fgetc(fp))
+        {    
+            // if apostrophes, set character to z+1
+            if (character == '\'')
+            {
+				int zAsInt = (int)'z';
+                character = zAsInt + 1;
+            }
+
+            // If the character is not in trie...
+            if (currentNode->children[character - 'a'] == NULL)
+            {
+                // ... malloc a new node and go there
+                currentNode->children[character - 'a'] = malloc(sizeof(trie));
+                currentNode = currentNode->children[character - 'a'];
+	        }
+	        else // ... but if it is, go to the existing node
+            {
+                currentNode = currentNode->children[character - 'a'];
+	        }
+	    }
+
+        currentNode->isAWord = true;
+        numberOfWords++;
+    }
+    fclose(fp);
+    return true;
 }
 
 /**
- * Returns number of words in dictionary if loaded else 0 if not yet loaded.
+ * Returns number of words in dictionary if loaded.
  */
 unsigned int size(void)
 {
-    // TODO
     return 0;
 }
 
+
 /**
- * Unloads dictionary from memory.  Returns true if successful else false.
- */
+ * Unloads dictionary from memory
+*/
 bool unload(void)
 {
-    // TODO
     return false;
 }
